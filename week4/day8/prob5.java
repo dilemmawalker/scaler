d2900939dded15ad94d0c3ff160114eaaa7f6125
yash handa <yashhanda500@gmail.com>
2021-02-22 01:02:05 +0530

    updaet

--- week4/day8/prob5.java
+++ week4/day8/prob5.java
@@ -1,14 +1,28 @@
 public class Solution {
     public int solve(int a, int b) {
-        long temp=0l;
-         temp=a+b;
-        a=Math.max(a,b);
-        b=(int)(temp-a);
-       
-        if(a==b)
-        return a;
-
-        return a-b;
+        int m=b;
+        b=b-2;
+        if(b==0){
+            // if(a==0)
+            // return 0;
+        return 1;
+        }
+        return sol(a,b,m);
     }
-    
+
+    public int sol(int a,int b,int m){
+
+        if(b==1){
+            return a%m;
+        }
+        long aa=0l;
+        int ans=sol(a,b/2,m);
+        if(b%2==0){
+            aa=(ans%m*ans%m)%m;
+        }
+        else{
+            aa=(ans%m*(ans%m*a%m)%m)%m;
+        }
+        return (int)(aa%m);
+    }
 }
