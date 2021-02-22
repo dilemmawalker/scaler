379a37a6666710f510b6dea9b031249f31ba7588
yash handa <yashhanda500@gmail.com>
2021-02-22 23:08:40 +0530

    update

---
+++ week4/day8/prob6.java
@@ -0,0 +1,17 @@
+boolean print(int i,int n,int ans[],int k,int sum){
+
+    if(i==n){
+        if(sum>k)
+        return true;
+        else
+        return false;
+    }
+
+    boolean res=false;
+  
+        int a=ans[i];
+        res=res||print(i+1,n,ans,k,sum);
+        res=res||print(i+1,n,ans,k,sum+a);
+
+    return res;
+}
