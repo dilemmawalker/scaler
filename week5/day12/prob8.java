32ddebb58fb76029622da4ca7d167af2b4a7071d
yash handa <yashhanda500@gmail.com>
2021-03-10 17:25:52 +0530

    update

--- week5/day12/prob8.java
+++ week5/day12/prob8.java
@@ -1,5 +1,22 @@
 public class Solution {
-    public int solve(int a) {
-        
+    public int solve(int ele) {
+        int a=0,b=0;
+        int pos=0;
+        for(int i=0;i<32;i++){
+            int mask=(1<<i);
+            if((ele & mask)!=0){
+                pos=i;
+            }
+        }
+
+        int y=(1<<(pos+1));
+        int x=0;
+        for(int i=0;i<pos;i++){
+            int mask=(1<<i);
+            if((ele & mask)==0){
+                x|=mask;
+            }
+        }
+        return x^y;
     }
 }
