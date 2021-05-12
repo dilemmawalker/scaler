f5439f018209c1bf7936ae9d993c89486c3a5aef
yash handa <yashhanda500@gmail.com>
2021-05-12 22:21:36 +0530

    update

---
+++ week16/day50/prob7.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String multiple(int a) {
+        int[]vis=new int[n];
+        int[]val=new int[n];
+        int[]parent=new int[n];
+
+        Queue<Integer>que=new LinkedList<>();
+        que.push(1);
+
+         while(que.size()!=0 && )
+    }
+}
