567be42c62c33cec7e397e380849e8d651a12345
yash handa <yashhanda500@gmail.com>
2021-05-17 14:30:17 +0530

    update

---
+++ week17/day55/prob3.java
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode A) {
+    }
+}
