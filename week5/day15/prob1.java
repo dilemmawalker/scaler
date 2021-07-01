09b0864b462cdd5e29d7dc7065b53577fd955843
Yash Handa <yashhanda500@gmail.com>
2021-07-01 23:30:44 +0530

    update

--- week5/day15/prob1.java
+++ week5/day15/prob1.java
@@ -1,38 +1,45 @@
-public class Solution {
-    public static int[][] permute(int[] arr) {
-         solve(arr,0,new ArrayList<Integer>());
-         int n=ans.size();
+public class Solution {   ///bekaar, put all ans in hashset & then automatically we get 
+                            //non repeated ans.....not a great soln!!!
+    public int[][] permute(int[] arr) {
+        ArrayList<Integer>ar=new ArrayList<>();
+        ArrayList<ArrayList<Integer>>final_ans=new ArrayList<>();
+        solve(arr,0,ar,final_ans);
+
+        int n=final_ans.size();
          int m=arr.length;
          int[][]anss=new int[n][m];
          for(int i=0;i<n;i++){
              for(int j=0;j<m;j++){
-                 anss[i][j]=ans.get(i).get(j);
+                 anss[i][j]=final_ans.get(i).get(j);
              }
          }
          return anss;
     }
-    public static ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
-    public static void solve(int[]arr,int idx,ArrayList<Integer>ar){
-        if(idx==arr.length){
+
+    public static void solve(int[]arr,int idx,ArrayList<Integer>ar,ArrayList<ArrayList<Integer>>final_ans){
+        int n=arr.length;
+        if(idx==n){
             ArrayList<Integer>temp=new ArrayList<>();
-            for(int i=0;i<arr.length;i++){
-                temp.add(ar.get(i));
+            for(int j=0;j<ar.size();j++){
+                temp.add(ar.get(j));
             }
-            ans.add(temp); 
+            final_ans.add(temp);
             return;
         }
-        HashSet<Integer>map=new HashSet<>();
-        for(int i=0;i<arr.length;i++){
+
+    HashSet<Integer>map=new HashSet<>();
+        for(int i=0;i<n;i++){
             if(arr[i]!=Integer.MAX_VALUE && !map.contains(arr[i])){
                 int val=arr[i];
-                map.add(val);
                 arr[i]=Integer.MAX_VALUE;
                 ar.add(val);
-                solve(arr,idx+1,ar);
+                map.add(val);
+                solve(arr,idx+1,ar,final_ans);
                 ar.remove(ar.size()-1);
+                map.remove(val);
                 arr[i]=val;
             }
         }
-        return;
+
     }
 }
