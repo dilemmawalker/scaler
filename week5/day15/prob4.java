cf9d928a5e2c5fb2b9dfaa7d22838efd2d540054
Yash Handa <yashhanda500@gmail.com>
2021-07-03 12:04:26 +0530

    gray code

---
+++ week5/day15/prob4.java
@@ -0,0 +1,17 @@
+public class Solution {
+	public ArrayList<Integer> grayCode(int a) {
+	    ArrayList<Integer>arr=new ArrayList<>();
+        arr.add(0);
+        arr.add(1);
+        solve(arr,a);
+        return arr;
+	}
+    public static void solve(ArrayList<Integer>arr,int n){
+        for(int i=1;i<n;i++){
+            int s=arr.size();
+            for(int j=s-1;j>=0;j--){
+                arr.add(arr.get(j) + (1<<i))
+            }
+        }
+    }
+}
