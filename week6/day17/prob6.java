6d9e5a22e567b260831565a4c0c07034136dfc63
Yash Handa <yashhanda500@gmail.com>
2021-08-14 15:20:06 +0530

    squareful array

--- week6/day17/prob6.java
+++ week6/day17/prob6.java
@@ -5,40 +5,54 @@ public class Solution {
         int vis=0;
         ArrayList<Integer>ar=new ArrayList<>();
 
-        int ans=0;
+         int ans=0;
+         if(n==1){
+             int a=(int)Math.sqrt(n);
+             if((a*a)==arr[0])
+             return 1;
+             else
+             return 0;
+         }
+        
         sol(arr,0,vis,ans,new ArrayList<Integer>(),new ArrayList<Integer>());
+        // System.out.println(ar);
         return ans;
     }
-    public void sol(int[]arr,int p,int vis,int ans,ArrayList<Integer>ar,ArrayList<Integer>index){
+    public int anss=0;
+    public int sol(int[]arr,int p,int vis,int ans,ArrayList<Integer>ar,ArrayList<Integer>index){
         if(p==arr.length){
             //check
            ans++;
+           anss++;
+           return 1;
         }
 
         int n=arr.length;
+        int a=0;
         for(int i=0;i<n;i++){
             if((vis & (1<<i))==0 ){
-                if(ar.size()==0 ||(ar.get(ar.size()-1)!=arr[i]) || (vis & (1<<(index.get(index.size()-1))))!=0 ){
+                if(i==0 || (arr[i-1]!=arr[i]) || (vis & (1<<(i-1)))!=0 ){
                 if(ar.size()!=0){
-                int p=ar.get(ar.size()-1);
-                int c=ar.get(i);
-                int sum=p+c;
+                int pr=ar.get(ar.size()-1);
+                int c=arr[i]; 
+                int sum=pr+c;
 
                 int val=(int)Math.sqrt(sum);
-                if((val*va)l!=sum)
+                if((val*val)!=sum)
                 continue;
                 }
 
                 vis|=(1<<i);
                 ar.add(arr[i]);
                 index.add(i);
-                sol(arr,p+1,vis,ans,ar,index);
+                a+=sol(arr,p+1,vis,ans,ar,index);
                 index.remove(index.size()-1);
                 ar.remove(ar.size()-1);
                 vis &=(~(1<<i));
                 }
                 }
             }
+            return a;
         }
     }
 
