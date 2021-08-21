518fa9b366591f2b9d260631d5a63299287114cd
Yash Handa <yashhanda500@gmail.com>
2021-08-21 12:27:45 +0530

    sum the difference

--- week7/day21/prob2.java
+++ week7/day21/prob2.java
@@ -5,14 +5,37 @@ public class Solution {
 
         long sum=0l;
         long s=0l;
+        int mod=(int)Math.pow(10,9)+7;
         for (int i=0;i<n;i++){
             int a=arr.get(i);
             
-            sum=(sum+(a*(int)(Math.pow(2,i)))%((int)Math.pow(10,9)+7))%((int)Math.pow(10,9)+7);
-            sum%=((int)Math.pow(10,9)+7);
-            s=(s+(a*(int)(Math.pow(2,n-i-1)))%((int)Math.pow(10,9)+7))%((int)Math.pow(10,9)+7);
-            s%=((int)Math.pow(10,9)+7);
+            sum= (sum+((a*power(2,i,mod)*1l + mod)%mod + mod)%mod + mod)%mod;
+            s= (s+((a*power(2,n-i-1,mod)*1l + mod)%mod + mod)%mod + mod)%mod;
         }
-        return (int)(sum-s);
+        return (int)(sum-s+mod)%mod;
     }
+
+     int power(int x, int y, int p){
+  
+    int res = 1; // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+    // equal to p
+ 
+    if (x == 0)
+      return 0; // In case x is divisible by p;
+ 
+    while (y > 0)
+    {
+ 
+      // If y is odd, multiply x with result
+      if ((y & 1) != 0)
+        res = (res * x) % p;
+ 
+      // y must be even now
+      y = y >> 1; // y = y/2
+      x = (x * x) % p;
+    }
+    return res;
+  }
 }
