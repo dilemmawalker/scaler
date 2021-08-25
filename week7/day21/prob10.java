3c5f9345018b4fe730717771199b3b36354ac829
Yash Handa <yashhanda500@gmail.com>
2021-08-25 12:34:31 +0530

    Min Swaps 2

---
+++ week7/day21/prob10.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int solve(int[] arr) {
+        int n=arr.length;
+        int[]idxarr=new int[n];
+
+        for(int i=0;i<n;i++){
+            int a=arr[i];
+            idxarr[a]=i;
+        }
+        int c=0;
+
+        for(int i=0;i<n;i++){
+            int a=arr[i];
+            if(i!=a){
+                c++;
+                int b=idxarr[i];
+                swap(arr,i,b);
+                swap(idxarr,i,a);
+            }
+        }
+        return c;
+    }
+    public void swap(int []arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
