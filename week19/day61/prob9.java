a73edb0a62b3c72a2a286e1add677b441cd41e14
Yash Handa <yashhanda500@gmail.com>
2021-09-10 23:59:08 +0530

    Postorder Traversal

--- week19/day61/prob9.java
+++ week19/day61/prob9.java
@@ -11,7 +11,7 @@
  *     }
  * }
  */
-public class Solution {
+public class Solution {                             //Postorder Traversal
     public int[] postorderTraversal(TreeNode root) {
 
  ArrayList<Integer>arr=new ArrayList<>();
