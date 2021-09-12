1f3a805c57764946147138f2b4034709d14ecad4
Yash Handa <yashhanda500@gmail.com>
2021-09-12 13:04:38 +0530

    Serialize Binary Tree

---
+++ week20/day63/prob6.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {                         //Serialize Binary Tree
+    public int[] solve(TreeNode root) {
+       ArrayList<ArrayList<Integer>>arr=new ArrayList<>();
+        sol(root,arr,0);
+        int n=arr.size();
+        int s=0;
+        int c=0;
+        for(int i=0;i<n;i++){
+            c+=arr.get(i).size();
+        }
+        int[]ar=new int[c];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            int m=arr.get(i).size();
+            for(int j=0;j<m;j++){
+                ar[idx++]=arr.get(i).get(j);
+            }
+        }
+        return ar;
+    }
+    public void sol(TreeNode root,ArrayList<ArrayList<Integer>>arr,int idx){
+
+        ArrayList<Integer>temp=new ArrayList<>();
+        if(idx<arr.size())
+        temp=arr.remove(idx);
+        
+        if(root==null){
+            temp.add(-1);
+            arr.add(idx,temp);
+        return;
+        }
+
+        temp.add(root.val);
+        arr.add(idx,temp);
+
+        sol(root.left,arr,idx+1);
+        sol(root.right,arr,idx+1);
+    }
+}
