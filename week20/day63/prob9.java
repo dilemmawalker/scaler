84e60fffcb8abf605b515c8a872d1aa173148b8f
Yash Handa <yashhanda500@gmail.com>
2021-09-12 16:10:45 +0530

    Right view

---
+++ week20/day63/prob9.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {                             //Right view
+    public int[] solve(TreeNode root) {
+       ArrayList<ArrayList<Integer>>arr=new ArrayList<>();
+        sol(root,arr,0);
+        int n=arr.size();
+        int s=0;
+        int[]ar=new int[n];
+        
+        for(int i=0;i<n;i++){
+            int m=arr.get(i).size();
+           ar[i]=arr.get(i).get(m-1);
+        }
+        return ar;
+    }
+    public void sol(TreeNode root,ArrayList<ArrayList<Integer>>arr,int idx){
+         if(root==null){
+            return;
+        }
+
+        ArrayList<Integer>temp=new ArrayList<>();
+        if(idx<arr.size())
+        temp=arr.remove(idx);
+        
+        temp.add(root.val);
+        arr.add(idx,temp);
+
+        sol(root.left,arr,idx+1);
+        sol(root.right,arr,idx+1);
+    }
+}
